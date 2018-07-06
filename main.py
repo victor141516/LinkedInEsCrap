89a9933e78bed3a7f2f6dbc744bd6cd6f6f57c29
Víctor <victor@shuttlecloud.com>
2018-07-06 18:37:32 +0200

    redirect

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-from flask import Flask, request
+from flask import Flask, request, redirect
 from linkedin import get_profile
 app = Flask(__name__)
 
 @app.route("/")
 def get_linkedin():
     email = request.args.get('e')
-    return get_profile(email)
+    try:
+        profile = get_profile(email)
+    except Exception:
+        return ':('
+    if request.args.has_key('r'):
+        return redirect("http://www.example.com", code=302)
+    else:
+        return profile
 
 if __name__ == "__main__":
     app.run('0.0.0.0', 9988, debug=True)
