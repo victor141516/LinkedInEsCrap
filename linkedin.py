7f26a42e3575a05c125e36971598e78a39073629
Víctor <victor@shuttlecloud.com>
2018-07-07 17:51:21 +0200

    Add interface && improvements

--- linkedin.py
+++ linkedin.py
@@ -1,57 +1,89 @@
 from bs4 import BeautifulSoup
-import config
 import json
 import requests
-import sys
 import urllib.parse
 
-def buildSearchUrl(term):
-    return 'https://www.linkedin.com/voyager/api/search/cluster?blendedSrpEnabled=true&count=10&guides=List()&keywords={}&origin=GLOBAL_SEARCH_HEADER&q=guided&start=0'.format(urllib.parse.quote_plus(term))
+
+class Scrapper(object):
+    def __init__(self, email, password):
+        super(Scrapper, self).__init__()
+        self.email = email
+        self.password = password
+        self.session = requests.Session()
+        self.login()
+
+    def login(self):
+        html = self.session.get('https://www.linkedin.com/').text
+        soup = BeautifulSoup(html, 'html.parser')
+        csrf = soup.select_one('#loginCsrfParam-login').attrs['value']
+
+        self.session.post('https://www.linkedin.com/uas/login-submit', data={
+            'session_key': self.email,
+            'session_password': self.password,
+            'isJsEnabled': False,
+            'loginCsrfParam': csrf
+        })
 
 
-def get_profile(email):
-    session = requests.Session()
-    html = session.get('https://www.linkedin.com/').text
-    soup = BeautifulSoup(html, 'html.parser')
-    csrf = soup.select_one('#loginCsrfParam-login').attrs['value']
-
-    session.post('https://www.linkedin.com/uas/login-submit', data={
-        'session_key': config.SCRAP_EMAIL,
-        'session_password': config.SCRAP_PASS,
-        'isJsEnabled': False,
-        'loginCsrfParam': csrf
-    })
-    trying = session.get('https://www.linkedin.com/sales/gmail/profile/proxy/' + email)
-    if (trying.url.startswith('https://www.linkedin.com/in/')):
-        return trying.url
-    html = session.get('https://www.linkedin.com/sales/gmail/profile/viewByEmail/' + email).text
-    soup = BeautifulSoup(html, 'html.parser')
-
-    user_id = soup.select_one('.li-img-profile-large').attrs['src'].split('/profile-displayphoto')[0].split('/')[-1]
-
-    terms = []
-    for selector in ['.li-user-name > a', '.li-user-title']:
-        try:
-            terms.append(soup.select_one(selector).text)
-        except Exception:
-            pass
-    term = ' '.join(terms)
-
-    raw = session.get(buildSearchUrl(term), headers={'csrf-token': session.cookies['JSESSIONID'][1:-1]}).text
-    search_result = json.loads(raw)
-
-    profile_id = None
-    for each1 in search_result['elements']:
-        for each2 in each1['elements']:
-            if user_id in each2['hitInfo']['com.linkedin.voyager.search.SearchProfile']['miniProfile']['picture']['com.linkedin.common.VectorImage']['rootUrl']:
-                profile_id = each2['hitInfo']['com.linkedin.voyager.search.SearchProfile']['miniProfile']['publicIdentifier']
-
-    if profile_id is not None:
-        return 'https://www.linkedin.com/in/' + profile_id
+    def retry_login(tries=3):
+        def wrapped(f):
+            def f_retry(self, *args, **kwargs):
+                h_tries = tries
+                while True:
+                    try:
+                        return f(self, *args, **kwargs)
+                    except Exception as e:
+                        print('retry')
+                        self.login()
+                        if h_tries > 1:
+                            h_tries -= 1
+                        else:
+                            raise(e)
+            return f_retry
+        return wrapped
 
 
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        sys.exit()
-    email = sys.argv[1]
-    print(get_profile(email))
+    def _fast_method(self, email_to_get):
+        trying = self.session.get(f'https://www.linkedin.com/sales/gmail/profile/proxy/{email_to_get}')
+        if (trying.url.startswith('https://www.linkedin.com/in/')):
+            return trying.url
+        else:
+            return False
+
+    def _safe_method(self, email_to_get):
+        def buildSearchUrl(term):
+            return 'https://www.linkedin.com/voyager/api/search/cluster?blendedSrpEnabled=true&count=10&guides=List()&keywords={}&origin=GLOBAL_SEARCH_HEADER&q=guided&start=0'.format(urllib.parse.quote_plus(term))
+
+        html = self.session.get(f'https://www.linkedin.com/sales/gmail/profile/viewByEmail/{email_to_get}').text
+        soup = BeautifulSoup(html, 'html.parser')
+
+        user_id = soup.select_one('.li-img-profile-large').attrs['src'].split('/profile-displayphoto')[0].split('/')[-1]
+
+        terms = []
+        for selector in ['.li-user-name > a', '.li-user-title']:
+            try:
+                terms.append(soup.select_one(selector).text)
+            except Exception:
+                pass
+        term = ' '.join(terms)
+
+        raw = self.session.get(buildSearchUrl(term), headers={'csrf-token': self.session.cookies['JSESSIONID'][1:-1]}).text
+        search_result = json.loads(raw)
+
+        profile_id = None
+        for each1 in search_result['elements']:
+            for each2 in each1['elements']:
+                if user_id in each2['hitInfo']['com.linkedin.voyager.search.SearchProfile']['miniProfile']['picture']['com.linkedin.common.VectorImage']['rootUrl']:
+                    profile_id = each2['hitInfo']['com.linkedin.voyager.search.SearchProfile']['miniProfile']['publicIdentifier']
+
+        if profile_id is not None:
+            return f'https://www.linkedin.com/in/{profile_id}'
+
+
+    @retry_login(3)
+    def get_profile(self, email_to_get):
+        fast = self._fast_method(email_to_get)
+        if fast:
+            return fast
+        else:
+            return self._safe_method(email_to_get)
--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-from flask import Flask, request, redirect
-from linkedin import get_profile
-app = Flask(__name__)
+import config
+from flask import Flask, request, redirect, send_from_directory
+from linkedin import Scrapper
+from werkzeug.exceptions import NotFound
 
-@app.route("/")
+app = Flask(__name__)
+linkedin_scrapper = Scrapper(config.SCRAP_EMAIL, config.SCRAP_PASS)
+
+
+@app.route("/get")
 def get_linkedin():
     email = request.args.get('e')
     try:
-        profile = get_profile(email)
+        profile = linkedin_scrapper.get_profile(email)
     except Exception as e:
         print(e)
         if 'err' in request.args:
@@ -17,6 +22,20 @@ def get_linkedin():
         return redirect(profile, code=302)
     else:
         return profile
+
+
+@app.route("/")
+def serve_index():
+    return send_from_directory('static', 'index.html')
+
+
+@app.route("/static/<path:path>")
+def serve_static(path):
+    try:
+        return send_from_directory('static', path)
+    except NotFound as e:
+        return send_from_directory('static', 'index.html')
+
 
 if __name__ == "__main__":
     app.run('0.0.0.0', 9988, debug=True)
