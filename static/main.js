7e2f4ef75a3e67a52826f540ff5c63745e3fd016
Víctor <victor@shuttlecloud.com>
2018-07-10 17:30:44 +0200

    fixes

--- static/main.js
+++ static/main.js
@@ -1,5 +1,5 @@
-(function(){
+document.addEventListener("DOMContentLoaded", function(event) {
     if (location.search === '?errorText') {
         document.getElementById('error-text').hidden = false;
     }
-})();
+});
